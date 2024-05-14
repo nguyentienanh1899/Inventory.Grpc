fe91c0196a1d25caf9223adfc55d32f8a13cfcea
AnhNguyen <anh.nguyen17@niteco.se>
2024-05-14 13:55:57 +0700

    Add project files.

---
+++ Inventory.Grpc/Services/ServiceExtentions.cs
@@ -0,0 +1,42 @@
+﻿using Infrastructure.Extensions;
+using Inventory.Grpc.Repositories;
+using Inventory.Grpc.Repositories.Interfaces;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Services
+{
+    public static class ServiceExtentions
+    {
+        public static IServiceCollection AddConfigurationSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var databaseSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            services.AddSingleton(databaseSettings);
+            return services;
+        }
+
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
+        }
+
+        public static void ConfigureMongoDbClient(this IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient>(
+                new MongoClient(GetMongoConnectionString(services)))
+                .AddScoped(x => x.GetService<IMongoClient>()?.StartSession());
+        }
+
+        private static string GetMongoConnectionString(this IServiceCollection services)
+        {
+            var settings = services.GetOptions<MongoDbSettings>(nameof(MongoDbSettings));
+            if(settings == null || string.IsNullOrEmpty(settings.ConnectionString)) {
+                throw new ArgumentNullException("DatabaseSettings is not configured");
+            }
+
+            var databaseName = settings.DatabaseName;
+            var mongoDbConnectionString = settings.ConnectionString + "/" + databaseName + "?authSource=admin";
+            return mongoDbConnectionString;
+        }
+    }
+}
